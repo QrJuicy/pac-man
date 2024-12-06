cf1d211345931be9ef657d4d5413a8a9c2b8f62c
QrJuicy <89000905+QrJuicy@users.noreply.github.com>
2024-12-06 11:48:18 +0800

    made queue for ghost activation

--- Pac Man/Scenes/Ghosts.gd
+++ Pac Man/Scenes/Ghosts.gd
@@ -1,9 +1,31 @@
 extends Node
 
+var ghost_global_clock = 0
+
+@export_group("Scarlet")
+@export var scarlet_ghost : Area2D
+@export var scarlet_start_time : float
+
+@export_group("Blush")
+@export var blush_ghost : Area2D
+@export var blush_start_time : float
+
+@export_group("Azure")
+@export var azure_ghost : Area2D
+@export var azure_start_time : float
+
+@export_group("Amber")
+@export var amber_ghost : Area2D
+@export var amber_start_time : float
+
 @onready var player = $"../PacMan" as Player
 
 func _ready():
 	player.player_died.connect(reset_ghosts)
+	
+func _process(delta):
+	ghost_global_clock+=1
+	ghost_activation_stack()
 
 func reset_ghosts(lifes):	
 	var ghosts = get_children() as Array[Ghost]
@@ -17,4 +39,12 @@ func reset_ghosts(lifes):
 		for ghost in ghosts:
 			ghost.setup()
 	
-
+func ghost_activation_stack():
+	if ghost_global_clock == scarlet_start_time:
+		scarlet_ghost.process_mode = Node.PROCESS_MODE_INHERIT
+	if ghost_global_clock == blush_start_time:
+		blush_ghost.process_mode = Node.PROCESS_MODE_INHERIT
+	if ghost_global_clock == azure_start_time:
+		azure_ghost.process_mode = Node.PROCESS_MODE_INHERIT
+	if ghost_global_clock == amber_start_time:
+		amber_ghost.process_mode = Node.PROCESS_MODE_INHERIT
